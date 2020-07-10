1c7d263a04c90f3127e8f8e44b9600497bc9851c
marcusungarette <marcusungarette@gmail.com>
2020-07-10 19:40:36 -0300

    Yup Library added and started setup the validation schema:name,email,password on the SignUp page

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -19,7 +19,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
-
+  // eslint-disable-next-line
   const { fieldName, defaultValue, error, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
